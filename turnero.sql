bba31a0220951063dc80b23350d545ae14a7b533
solsv7 <solsv7@gmail.com>
2025-05-05 21:44:26 -0300

    Normalizo finales de línea en turnero.sql

--- turnero.sql
+++ turnero.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-05-2025 a las 21:44:12
+-- Tiempo de generación: 06-05-2025 a las 02:39:11
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -38,6 +38,18 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `RegisterClient` (IN `p_email` VARCH
 
     SET new_user_id = LAST_INSERT_ID();
 
+
+    INSERT INTO datos_personales (id_usuario, dni, celular)
+    VALUES (new_user_id, p_dni, p_celular);
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `RegisterUser` (IN `p_email` VARCHAR(255), IN `p_password` VARCHAR(255), IN `p_nombre` VARCHAR(100), IN `p_dni` VARCHAR(20), IN `p_celular` VARCHAR(20), IN `p_id_rol` INT)   BEGIN
+    DECLARE new_user_id INT;
+
+    INSERT INTO usuarios (email, password, nombre, id_rol, id_estado)
+    VALUES (p_email, p_password, p_nombre, p_id_rol, 1);
+
+    SET new_user_id = LAST_INSERT_ID();
 
     INSERT INTO datos_personales (id_usuario, dni, celular)
     VALUES (new_user_id, p_dni, p_celular);
@@ -113,7 +125,8 @@ CREATE TABLE `datos_personales` (
 --
 
 INSERT INTO `datos_personales` (`id_usuario`, `dni`, `celular`) VALUES
-(4, '46999888', '1133445566');
+(4, '46999888', '1133445566'),
+(6, '41991328', '2364310386');
 
 -- --------------------------------------------------------
 
@@ -197,7 +210,8 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `email`, `password`, `id_rol`, `nombre`, `id_estado`) VALUES
-(4, 'lau@example.com', '$2b$10$JNw8UvDnif80vSmv1aCdqe0CJzLq.dZ5DyFagSAWUE2UGvPg6u9RS', 3, 'Lauti', 2);
+(4, 'lau@example.com', '$2b$10$JNw8UvDnif80vSmv1aCdqe0CJzLq.dZ5DyFagSAWUE2UGvPg6u9RS', 3, 'Lauti', 2),
+(6, 'solv7@gmail.com', '$2b$10$JucDwzLHnEQzABUNumPYf.IgNDbtqD9WfPm.K1l2A8ve5j8d35bny', 2, 'Sol', 1);
 
 -- --------------------------------------------------------
 
@@ -341,7 +355,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
